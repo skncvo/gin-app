799c63c8e1cf71901b65fecebf2db688661de5c7
skncvo <wm3233@naver.com>
2025-06-30 15:48:39 +0900

    설명 추가

--- config/database.go
+++ config/database.go
@@ -16,6 +16,7 @@ func ConnectDB() {
 		dbFile = "app.db"
 	}
 
+	// &gorn.Config라는 구조체 주소를 열어서 {} 사용
 	database, err := gorm.Open(sqlite.Open(dbFile), &gorm.Config{})
 
 	if err != nil {
--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 type User struct {
+	// `` : go 에서 struct 묶을 때 백틱 사용
 	ID    uint   `gorm:"primaryKey" json:"id"`
 	Name  string `json:"name"`
 	Email string `gorm:"unique" json:"email"`
